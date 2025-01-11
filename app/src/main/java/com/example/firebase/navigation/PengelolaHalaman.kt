233ebf23176a0b0b88a03398f7a41731875e6abc
ijaamm <ijammusyaf@gmail.com>
2025-01-11 20:02:15 +0700

    membuat pengelola halaman untuk destinasi home

---
+++ app/src/main/java/com/example/firebase/navigation/PengelolaHalaman.kt
@@ -0,0 +1,33 @@
+package com.example.firebase.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.firebase.ui.home.pages.HomeScreen
+import com.example.firebase.ui.home.pages.InsertScreen
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ){
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToitemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+    }
+}
