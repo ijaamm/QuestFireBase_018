bcb2e6cf1add552714ff3171398a7a83867636fa
ijaamm <ijammusyaf@gmail.com>
2025-01-11 19:12:39 +0700

    menambah dan melengkapi function

--- app/src/main/java/com/example/firebase/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/firebase/repository/NetworkRepositoryMhs.kt
@@ -6,12 +6,17 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkRepositoryMhs (
     private val firestore: FirebaseFirestore
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try{
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        }catch (e : Exception){
+            throw Exception("Gagal menambahkan dara mahasiswa :${e.message}")
+        }
     }
 
     override fun getAllMhs(): Flow<List<Mahasiswa>>  = callbackFlow {
@@ -30,16 +35,40 @@ class NetworkRepositoryMhs (
         }
     }
 
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
+    override fun getMhs(nim: String): Flow<Mahasiswa> = callbackFlow {
+        val mhsDocument = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener { value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(Mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose {
+            mhsDocument.remove()
+        }
     }
 
     override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        }catch (e : Exception){
+            throw Exception("Gagal menghapus data mahasiswa :${e.message}")
+        }
     }
 
     override suspend fun updateMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .set(mahasiswa)
+                .await()
+        }catch (e: Exception){
+            throw Exception("Gagal mengupdate data mahasiswa :${e.message}")
+        }
     }
 
 }
